5f1c22c2581a1215a0a76faf612019969a514523
arilthon <arilthony@gmail.com>
2021-11-18 22:43:31 -0300

    scroll na pagina

--- main.js
+++ main.js
@@ -29,3 +29,32 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carousel slider switer*/
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*  Scrollreveal: mostra elementos quando der scroll na pagina*/
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header,#testimonials .testimonials
+#contact .text, #contact .links`,
+  { intervalos: 100 }
+)
